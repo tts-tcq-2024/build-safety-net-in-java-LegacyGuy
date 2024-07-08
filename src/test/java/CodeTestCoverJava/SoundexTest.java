dfb46d6eb357fe60595f10f9e421710396351a39
LegacyGuy <prathameshkashid26@gmail.com>
2024-07-08 10:52:22 +0530

    Update SoundexTest.java
    Added test cases

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -1,18 +1,35 @@
 package CodeTestCoverJava;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class SoundexTest {
 
     @Test
     public void testEmptyString() {
-       //  assertTrue(Soundex.generateSoundex("")=="");
+        assertEquals("", Soundex.generateSoundex(""));
     }
 
     @Test
     public void testSingleCharacter() {
-         //assertTrue(Soundex.generateSoundex("A")=="A000");
+        assertEquals("A000", Soundex.generateSoundex("A"));
+    }
+
+    @Test
+    public void testCommonNames() {
+        assertEquals("A261", Soundex.generateSoundex("Ashcraft"));
+        assertEquals("P236", Soundex.generateSoundex("Pfister"));
+        assertEquals("H555", Soundex.generateSoundex("Honeyman"));
+    }
+
+    @Test
+    public void testSimilarSoundingNames() {
+        assertEquals("B620", Soundex.generateSoundex("Bartosz"));
+        assertEquals("B620", Soundex.generateSoundex("Bartoz"));
+    }
+
+    @Test
+    public void testCaseInsensitivity() {
+        assertEquals(Soundex.generateSoundex("Ashcraft"), Soundex.generateSoundex("ashcraft"));
     }
 }
