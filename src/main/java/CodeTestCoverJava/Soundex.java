dffde4f57bdd34923df47bae753be0caa4e647de
LegacyGuy <prathameshkashid26@gmail.com>
2024-08-12 18:37:58 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -1,82 +1,110 @@
+
 package CodeTestCoverJava;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import java.util.Optional;
+
 
 public class Soundex {
 
-    private static final Map<Character, Character> CHAR_TO_DIGIT_MAP = createCharToDigitMap();
+    private static final HashMap<String, Character> soundexCodeMap = new HashMap<String, Character>() {
+        {
+            put("BFPV", '1');
+            put("CGJKQSXZ", '2');
+            put("DT", '3');
+            put("L", '4');
+            put("MN", '5');
+            put("R", '6');
+        }
+    };
 
-    private static Map<Character, Character> createCharToDigitMap() {
-        Map<Character, Character> map = new HashMap<>();
-        map.put('B', '1');
-        map.put('F', '1');
-        map.put('P', '1');
-        map.put('V', '1');
-        map.put('C', '2');
-        map.put('G', '2');
-        map.put('J', '2');
-        map.put('K', '2');
-        map.put('Q', '2');
-        map.put('S', '2');
-        map.put('X', '2');
-        map.put('Z', '2');
-        map.put('D', '3');
-        map.put('T', '3');
-        map.put('L', '4');
-        map.put('M', '5');
-        map.put('N', '5');
-        map.put('R', '6');
-        return map;
-    }
+    private static final Character INVALID_CHARACTER = '0';
+
+    private static final List<Character> vowels = Arrays.asList('A', 'E', 'I', 'O', 'U');
+    private static final List<Character> ignoreAppendCharacters = Arrays.asList('W', 'H', 'Y');
+
 
     public static String generateSoundex(String name) {
-        if (isInvalidName(name)) return "";
+        if (name == null || name.isEmpty()) {
+            return "";
+        }
 
-        StringBuilder soundexCode = new StringBuilder();
-        char firstChar = name.toUpperCase().charAt(0);
-        soundexCode.append(firstChar);
+        // Retain the first letter of the name and convert it to uppercase
+        StringBuilder soundex = createSoundexString(name.toUpperCase());
+        appendPadding(soundex);
 
-        processRemainingChars(name, soundexCode);
-
-        return padToFourCharacters(soundexCode).toString();
+        return soundex.substring(0, 4);
     }
 
-    private static boolean isInvalidName(String name) {
-        return name == null || name.isEmpty();
-    }
-
-    private static void processRemainingChars(String name, StringBuilder soundexCode) {
-        char prevDigit = mapCharToDigit(name.toUpperCase().charAt(0));
-
-        for (int i = 1; i < name.length(); i++) {
-            char currentChar = name.toUpperCase().charAt(i);
-            if (Character.isLetter(currentChar)) {
-                char currentDigit = mapCharToDigit(currentChar);
-                appendIfValid(soundexCode, currentDigit, prevDigit);
-                prevDigit = currentDigit;
+    private static StringBuilder createSoundexString(String name) {
+        char firstLetter = Character.toUpperCase(name.charAt(0));
+        StringBuilder soundex = new StringBuilder().append(Character.toUpperCase(firstLetter));
+        // Convert the rest of the name to uppercase and map to Soundex digits
+        for (int charracterIndex = 1; charracterIndex < name.length(); charracterIndex++) {
+            if (isValidCharacterForSoundex(name, charracterIndex)) {
+                soundex.append(getCodeForCharacterFromMap(name.charAt(charracterIndex)));
             }
         }
+        return soundex;
+
     }
 
-    private static void appendIfValid(StringBuilder soundexCode, char currentDigit, char prevDigit) {
-        if (isValidSoundexCharacter(currentDigit, prevDigit) && soundexCode.length() < 4) {
-            soundexCode.append(currentDigit);
+    private static Character getCodeForCharacterFromMap(Character alphabet) {
+        Character code = INVALID_CHARACTER;
+        Optional<String> selectedKey = soundexCodeMap.keySet().stream().filter(key -> key.indexOf(alphabet) != -1).findFirst();
+        if (selectedKey.isPresent()) {
+            code = soundexCodeMap.get(selectedKey.get());
+        }
+        return code;
+    }
+
+    private static boolean isValidCharacterForSoundex(String name, int index) {
+        List<Boolean> status = new ArrayList<>();
+        status.add(isAlpha(name, index));
+        status.add(!isVowel(name, index));
+        status.add(!isIgnoreCharacter(name, index));
+        status.add(!isConsecutiveCharacter(name, index));
+        status.add(!isPrefixedByIgnore(name, index));
+        return  !status.contains(false);
+
+
+    }
+
+
+    private static boolean isPrefixedByIgnore(String name, int index) {
+        boolean status = false;
+        if (isIgnoreCharacter(name, index-1) && index > 1) {
+            status = (getCodeForCharacterFromMap(name.charAt(index)) == getCodeForCharacterFromMap(name.charAt(index-2)));
+        }
+        return status;
+    }
+
+    private static void appendPadding(StringBuilder soundex) {
+        while (soundex.length() < 4) {
+            soundex.append('0');
         }
     }
 
-    private static char mapCharToDigit(char c) {
-        return CHAR_TO_DIGIT_MAP.getOrDefault(c, '0');
+
+
+    private static boolean isConsecutiveCharacter(String name, int index) {
+        return getCodeForCharacterFromMap(name.charAt(index)) == getCodeForCharacterFromMap(name.charAt(index-1));
     }
 
-    private static boolean isValidSoundexCharacter(char currentDigit, char prevDigit) {
-        return currentDigit != '0' && currentDigit != prevDigit;
+    private static boolean isIgnoreCharacter(String name, int index) {
+        return ignoreAppendCharacters.contains(name.charAt(index));
     }
 
-    private static StringBuilder padToFourCharacters(StringBuilder soundexCode) {
-        while (soundexCode.length() < 4) {
-            soundexCode.append('0');
-        }
-        return soundexCode;
+    private static boolean isAlpha(String name, int index) {
+        return Character.isAlphabetic(name.charAt(index));
     }
+
+    private static boolean isVowel(String name, int index) {
+        return vowels.contains(name.charAt(index));
+    }
+
 }
+
